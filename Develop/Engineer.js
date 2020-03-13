94dafb2226aa384455eb5cd0ebcec156ead86e08
James Geib <jpgeib2@gmail.com>
2020-03-12 18:21:35 -0700

    first commit

---
+++ Develop/Engineer.js
@@ -0,0 +1,19 @@
+const Employee = require("./Employee")
+
+
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        super(name, id, email);
+        this._github = github;
+    }
+
+    getGithub() {
+        return this._github;
+    }
+
+    getRole() {
+        return "Engineer";
+    }
+};
+
+module.exports = Engineer;
