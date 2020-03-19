eee88a864e671fd61b053643bdd0272cfe6c7241
James Geib <jpgeib2@gmail.com>
2020-03-18 17:29:04 -0700

    building generateHTML, making alterations to HTML files

--- Develop/Javascript/generateHTML.js
+++ Develop/Javascript/generateHTML.js
@@ -1,3 +1,24 @@
 const fs = require("fs");
 const path = require("path");
 
+const templateHTML = path.resolve(__dirname, "../HTML");
+
+const render = function(employees) {
+    const html = [];
+
+    html.push(employees.filter(employee => employee.getRole() === "Manager").map(manager => renderManager(manager)));
+};
+
+const renderManager = function(manager) {
+    let template = fs.readFileSync(path.resolve(templateHTML, "manager.html"), "utf8");
+    template = replacePlaceholders()
+}
+
+
+const renderTeam = function(html) {
+    const template = fs.readFileSync(path.resolve(templateHTML, "team.html"), "utf8");
+};
+
+const fillInfo = function() {
+    const 
+};
