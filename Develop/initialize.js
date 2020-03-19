f743330581de140d4c04a3760eb22f9eae9600a1
James Geib <jpgeib2@gmail.com>
2020-03-18 17:04:47 -0700

    starting the generateHTML/initialize files

---
+++ Develop/initialize.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const Manager = require("./Javascript/Manager");
+const Intern = require("./Javascript/Intern");
+const Engineer = require("./Javascript/Engineer");
+
